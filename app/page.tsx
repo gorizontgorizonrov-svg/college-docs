c0ebc0aeeb59cda5500274e164ab9588b37dbcf3
gorizontgorizonrov-svg <gprizontgorizonrov@gmail.com>
2026-06-22 09:48:57 +0600

    удалена титульная страница, старт сразу с /login

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { redirect } from "next/navigation";
 import { auth } from "@/auth";
-import LandingClient from "./LandingClient";
 
 export default async function Home() {
   const session = await auth();
-  if (session?.user) redirect("/dashboard");
-  return <LandingClient />;
+  if (session?.user) {
+    redirect("/dashboard");
+  }
+  redirect("/login");
 }
